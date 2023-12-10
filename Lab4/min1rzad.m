6275fe70c2cc737638f9c1758a105d5341de75bd
szymeqic <szym18kowalski@gmail.com>
2023-12-10 14:32:11 +0100

    dziala

--- Lab4/min1rzad.m
+++ Lab4/min1rzad.m
@@ -6,7 +6,9 @@ global tm tachom
 L = [X(1)]; %kp
 M = [X(2) 1]; %X(2) = Ts
 
-Ys = step(L,M, tm);
+sys = tf(L,M);
+
+Ys = step(sys,tm);
 wynik = sum((tachom-Ys).^2);
 
 end
--- Lab4/silnik_model.m
+++ Lab4/silnik_model.m
@@ -3,9 +3,27 @@ t = dane(:,1);
 zas = dane(:,2);
 tacho = dane(:,3);
 
-
+global tm tachom
 
 figure(1)
 plot(t,zas, t,tacho)
 title("Odczyt csv")
+
+int = 10;
+
+tm= t(39:7:410);
+tachom = tacho(39:7:410);
+
+tm = tm + abs(tm(1));
+tachom = tachom +abs(tachom(1));
+
+
+Xp = [1 1];
+Xf = fminsearch("min1rzad",Xp);
+
+Y = step(Xf(1), [Xf(2) 1], tm);
+
+figure(2)
+plot(tm,Y, tm, tachom, 'x')
+title("Model")
 
